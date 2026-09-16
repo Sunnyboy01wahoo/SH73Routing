e1c08b689e493ecfb9f5d1ad00e5039ced296d5b
Sunnyboy01wahoo <165205054+Sunnyboy01wahoo@users.noreply.github.com>
2026-09-16 14:45:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -43,13 +43,20 @@ basemap.src =
 // CANVAS
 // =====================================================
 
+function resizeCanvas()
+{
+    canvas.width =
+        window.innerWidth;
+
+    canvas.height =
+        window.innerHeight;
+}
+
+resizeCanvas();
+
 window.addEventListener(
     "resize",
-    () =>
-    {
-        resizeCanvas();
-        draw();
-    }
+    resizeCanvas
 );
 
 // =====================================================
